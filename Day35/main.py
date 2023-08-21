f3e4b5d18673ee1d3bb2a3cd722ca4250894861c
RyanWri <ranman37@gmail.com>
2023-08-21 11:18:58 +0300

    open weather api data with parser

--- Day35/main.py
+++ Day35/main.py
@@ -1,15 +1,25 @@
-import requests
+from typing import List
 from open_weather import OpenWeather
 from dotenv import load_dotenv
 import os
+
+
+def check_if_its_gonna_rain(hourly_data: List[dict]) -> bool:
+    for three_hour in hourly_data:
+        if three_hour["weather_id"] < 700:
+            return True
+
+    return False
 
 
 def main():
     load_dotenv()
     api_key = os.getenv("OPEN_WEATHER_API_KEY")
     open_weather = OpenWeather(api_key)
-    resp = open_weather.get_weather_for_city(32.085300, 34.781769)
-    print(resp)
+    lat, lng = 61.924110, 25.748152
+    hourly_data = open_weather.get_hourly_forecast_data(lat, lng)
+    parsed = open_weather.parse_hourly_forecast_data(hourly_data)
+    print(check_if_its_gonna_rain(parsed))
 
 
 if __name__ == "__main__":
--- Day35/open_weather.py
+++ Day35/open_weather.py
@@ -1,17 +1,40 @@
+from datetime import datetime
+from typing import List
 import requests
 
 
 class OpenWeather:
     def __init__(self, api_key):
         self.api_key = api_key
-        self.base_url = "https://api.openweathermap.org/data/2.5/"
+        self.base_url = "https://api.openweathermap.org"
 
     def get_weather_for_city(self, lat: float, lon: float) -> dict:
         params = dict(lat=lat, lon=lon, appid=self.api_key)
-        url = f'{self.base_url}/weather'
+        url = f'{self.base_url}/data/2.5/weather'
         try:
             response = requests.get(url=url, params=params)
             return response.json()
         except Exception as e:
             print(e)
             return {}
+
+    def get_hourly_forecast_data(self, lat: float, lon: float) -> dict:
+        params = dict(lat=lat, lon=lon, appid=self.api_key)
+        url = f'{self.base_url}/data/2.5/forecast'
+        try:
+            response = requests.get(url=url, params=params)
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            print(e)
+            return {}
+
+    def parse_hour(self, item: dict) -> dict:
+        ts = datetime.fromtimestamp(item["dt"]).isoformat()
+        weather_id = item["weather"][0]["id"]
+        weather = item["weather"][0]["main"]
+        description = item["weather"][0]["description"]
+        return dict(ts=ts, weather=weather, description=description, weather_id=weather_id)
+
+    def parse_hourly_forecast_data(self, data: dict) -> List[dict]:
+        return [self.parse_hour(item) for item in data["list"][:4]]
